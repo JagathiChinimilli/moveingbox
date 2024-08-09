065ea1e98b69f3360228b5ba3b9f09a23bd03212
JagathiChinimilli <160092292+JagathiChinimilli@users.noreply.github.com>
2024-08-09 15:18:58 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,11 @@
+let score=0;
+let box=document.getElementById("box");
+let display=document.getElementById("score");
+
+box.addEventListener('click',function(){
+     score++;
+     display.textContent=score;
+     box.style.top=math.random()*90+"%";
+     box.style.left=math.random()*90+"%";
+});
+setTimeout(()=> alert("game over !your score is"+score),10000);
